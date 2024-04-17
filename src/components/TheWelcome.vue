3c7c59cd2738d9e6638094a787b4cf80ae006eeb
Björn Persson <bjorn.persson@knowit.se>
2024-04-17 19:54:42 +0200

    call repo and render a text

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>hej</div>
+  <div>
+    <h1>Hello Nortic</h1>
+    <p>{{ fact }}</p>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { BackendRepository } from "@/api/backend-repository";
+import { ref } from "vue";
 
+const fact = ref<string>("");
+
+const loadCatFact = () => {
+  BackendRepository.getCatFact().then((response) => {
+    fact.value = response.fact;
+  });
+};
+
+loadCatFact();
 </script>
